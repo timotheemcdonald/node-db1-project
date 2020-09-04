2203a910c4a6867afce666ad8527549f727cc5b3
timotheemcdonald <timotheemcdonald@gmail.com>
2020-09-04 18:33:26 -0400

    set-up

--- api/server.js
+++ api/server.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const { json } = require('express')
 
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.get("/api/accounts", (req,res) => {
+    
+    .then(value => {
+
+    })
+    .catch(error => {
+        res.status(500).json({ error: "Accounts couldn't be retrieved."})
+    })
+})
 
 module.exports = server;
